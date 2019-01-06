89b8da6bb6afdf97b3334b10ceea4aa5e948c148
3A532065 <weyna1649@gmail.com>
2019-01-07 03:09:03 +0800

    新增分支 Branch
    當想要測試新功能，但又怕把本來的程式弄壞
    希望隨時可以換回本來的程式
    這時就可以使用分支的功能

--- app/src/main/java/com/example/wayne/test1/Text1Activity.java
+++ app/src/main/java/com/example/wayne/test1/Text1Activity.java
@@ -2,6 +2,7 @@ package com.example.wayne.test1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class Text1Activity extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class Text1Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_text1);
+
+            Log.d("test","測試分支功能");
     }
 }
