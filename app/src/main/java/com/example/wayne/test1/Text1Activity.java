8d3855153004abd6f85acb50658a89e7dab248d4
3A532065 <weyna1649@gmail.com>
2019-01-07 02:53:55 +0800

    Android Studio的第一次Commit

---
+++ app/src/main/java/com/example/wayne/test1/Text1Activity.java
@@ -0,0 +1,15 @@
+package com.example.wayne.test1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Text1Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        String test ="測試一";
+        int XXX =1234;
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_text1);
+    }
+}
